81a5242ece0c49c4afa9bbc0097c3656c18c5104
Gui <psadatv@gmail.com>
2021-09-01 20:12:03 -0300

    fix bug

--- src/utils/restease.py
+++ src/utils/restease.py
@@ -1,9 +1,11 @@
 from resteasy import RESTEasy, json
 
+import sys
+
 def get_city_from_cep( cep: str ) -> str:
     cep = cep.replace('-', '')
     api = RESTEasy(endpoint=f'https://viacep.com.br/ws/{cep}/json',
                verify=True, cert=None, timeout=60,
                allow_redirects=True,
                encoder=json.dumps, decoder=json.loads, debug=False)
-    return api.get()['localidade']
+    return api.get()['localidade'].encode('iso-8859-1').decode('utf-8')
